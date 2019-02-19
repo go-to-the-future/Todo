68e7643f8cf978e8240b4559b7427c14b301335f
Kiyoto Ryuman <kiyokiyoabc@mac-the-end.local>
2019-02-19 15:33:38 +0900

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Kiyoto Ryuman on 2019/02/19.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
