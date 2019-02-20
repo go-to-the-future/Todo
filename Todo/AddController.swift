2c118b746989a575805eece7b26e8f08e3081b4e
Kiyoto Ryuman <kiyokiyoabc@mac-the-end.local>
2019-02-20 14:58:15 +0900

    first-commit

---
+++ Todo/AddController.swift
@@ -0,0 +1,38 @@
+//
+//  AddController.swift
+//  Todo
+//
+//  Created by Kiyoto Ryuman on 2019/02/19.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import UIKit
+
+//変数の設置
+var TodoKobetsunonakami = [String]()
+
+class AddController: UIViewController {
+    
+    //テキストフィールドの設定
+    @IBOutlet weak var TodoTextField: UITextField!
+    
+    //追加ボタンの設定
+    @IBAction func TodoAddButten(_ sender: Any) {
+        //変数に入力内容を入れる
+        TodoKobetsunonakami.append(TodoTextField.text!)
+        //追加ボタンを押したらフィールドを空にする
+        TodoTextField.text = ""
+        //変数の中身をUDに追加
+        UserDefaults.standard.set( TodoKobetsunonakami, forKey: "TodoList" )
+    }
+    
+    //最初からあるコード
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //最初からあるコード
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
